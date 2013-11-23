fd2a5d19290ef897eb36a6faaf8f09c83cf320dd
taazz <taazz@freemail.gr>
2013-11-23 17:23:50 +0200

    PostgreSQL and SQLite highlighters copied from lazarus trunc.

---
+++ Librarian/HighLighters/StrConst.pas
@@ -0,0 +1,41 @@
+unit StrConst;
+
+Interface
+
+ResourceString
+  //nuevo
+  SYNS_AttrSingleString         =  'Single Quoted String';
+  SYNS_AttrURI                  =  'URI';
+  SYNS_AttrVisitedURI           =  'Visited URI';
+  SYNS_AttrVrmlAppearance       =  'Vrml_Appearance';
+  SYNS_AttrVrmlAttribute        =  'Vrml_Attribute';
+  SYNS_AttrVrmlDefinition       =  'Vrml_Definition';
+  SYNS_AttrVrmlEvent            =  'Vrml_Event';
+  SYNS_AttrVrmlGrouping         =  'Vrml_Grouping';
+  SYNS_AttrVrmlInterpolator     =  'Vrml_Interpolator';
+  SYNS_AttrVrmlLight            =  'Vrml_Light';
+  SYNS_AttrVrmlNode             =  'Vrml_Node';
+  SYNS_AttrVrmlParameter        =  'Vrml_Parameter';
+  SYNS_AttrVrmlProto            =  'Vrml_Proto';
+  SYNS_AttrVrmlSensor           =  'Vrml_Sensor';
+  SYNS_AttrVrmlShape            =  'Vrml_Shape';
+  SYNS_AttrVrmlShape_Hint       =  'Vrml_Shape_Hint';
+  SYNS_AttrVrmlTime_dependent   =  'Vrml_Time_dependent';
+  SYNS_AttrVrmlViewpoint        =  'Vrml_Viewpoint';
+  SYNS_AttrVrmlWorldInfo        =  'Vrml_WorldInfo';
+  SYNS_AttrX3DDocType           =  'X3DDocType';
+  SYNS_AttrX3DHeader            =  'X3DHeader';
+
+  SYNS_FilterHP                 =  'HP48 Files (*.s,*.sou,*.a,*.hp)|*.S;*.SOU;*.A;*.HP';
+  SYNS_FilterRuby               =  'Ruby Files (*.rb;*.rbw)|*.rb;*.rbw';
+  SYNS_FilterRC                 =  'Resource Files (*.rc)|*.rc';
+  SYNS_FilterST                 =  'Structured Text Files (*.st)|*.st';
+  SYNS_FilterURI                =  'All Files (*.*)|*.*';
+  SYNS_FilterVrml97             =  'Vrml97/X3D World (*.wrl;*.wrml;*.vrl;*.vrml;*.x3d)|*.wrl;*.wrml;*.vrl;*.vrml;*.x3d';
+  SYNS_FilterX86Assembly        =  'x86 Assembly Files (*.asm)|*.asm';
+  SYNS_FilterCS                 =  'C# Files (*.cs)|*.cs';
+  SYNS_LangCS                   =  'CSharp';
+
+implementation
+
+end.
